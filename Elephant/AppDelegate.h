1a39ab1c64c27b698bc9fc7b84b9977c86a16a58
shumengye <shumeng.ye@gmail.com>
2013-10-25 20:52:39 +0200

    Initial Commit

---
+++ Elephant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Elephant
+//
+//  Created by Shumeng Ye on 25/10/13.
+//  Copyright (c) 2013 Shumeng Ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Elephant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Elephant
+//
+//  Created by Shumeng Ye on 25/10/13.
+//  Copyright (c) 2013 Shumeng Ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
